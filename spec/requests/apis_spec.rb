6845fe1f33053115c6b9d9a05c0a1625b4c35bff
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-06-02 19:37:23 +0300

    pretty json

--- spec/requests/apis_spec.rb
+++ spec/requests/apis_spec.rb
@@ -1,10 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Apis", type: :request do
-  describe "GET /apis" do
+  def pretty_json(json)
+    JSON.pretty_generate(JSON.parse(response.body))
+  end
+
+  describe "GET /category" do
+    let(:category) { FactoryBot.create :category }
     it "works! (now write some real specs)" do
-      get apis_path
+      get category_path(category.id, format: :json)
       expect(response).to have_http_status(200)
+      puts pretty_json(response.body)
     end
   end
 end
