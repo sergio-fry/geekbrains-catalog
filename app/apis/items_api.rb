681bc2ba7e24f4826b6f7d6d7f7c343d3e5c1958
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-06-03 10:05:10 +0300

    auth does not work

--- app/apis/items_api.rb
+++ app/apis/items_api.rb
@@ -4,8 +4,19 @@ class ItemsApi < Grape::API
   resource :items do
     helpers ::Helpers::ItemHelpers
 
+    helpers do
+      def current_user
+        @current_user ||= User.authorize!(env)
+      end
+
+      def authenticate!
+        error!('401 Unauthorized', 401) unless current_user
+      end
+    end
+
     desc "Items list"
     get do
+      authenticate!
       present Item.all, with: ItemSerializer
     end
 
@@ -15,6 +26,7 @@ class ItemsApi < Grape::API
     end
     route_param :id do
       get do
+        authenticate!
         ItemSerializer.new(item)
       end
     end
