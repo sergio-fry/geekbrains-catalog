125f56c3eb55f8fabc94b444e4e0b6d12db5c393
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-06-06 20:23:08 +0300

    sign in / out

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_185903) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_171842) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_185903) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_185903) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "default", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
