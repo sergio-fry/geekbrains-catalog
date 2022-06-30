05dd6ad96b42a5c7d9f85fe03c1759a8355b06a2
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-06-30 21:58:47 +0300

    refactor

--- app/external/imported_users.rb
+++ app/external/imported_users.rb
@@ -4,21 +4,28 @@ class ImportedUsers
   end
 
   def save
-    xlsx = Roo::Excelx.new(@file.path)
-    xlsx.sheet(0).parse(headers: true).each_with_index do |attrs, index|
+    rows.each_with_index do |attrs, index|
       next if index == 0
 
-      user = User.find_or_initialize_by(email: attrs["email"])
-      user.assign_attributes(
-        name: attrs["name"],
-        role: attrs["role"]
-      )
-
-      if user.new_record?
-        user.password = user.password_confirmation = "secret123"
-      end
-
-      user.save!
+      create_or_update_user attrs
     end
+  end
+
+  def rows
+    Roo::Excelx.new(@file.path).sheet(0).parse(headers: true)
+  end
+
+  def create_or_update_user(attrs)
+    user = User.find_or_initialize_by(email: attrs["email"])
+    user.assign_attributes(
+      name: attrs["name"],
+      role: attrs["role"]
+    )
+
+    if user.new_record?
+      user.password = user.password_confirmation = "secret123"
+    end
+
+    user.save!
   end
 end
