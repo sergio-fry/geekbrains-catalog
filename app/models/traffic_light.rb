d62857a51ecf94f395b2fa3299834e6951ab4d6a
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-07-04 21:10:27 +0300

    guards

--- app/models/traffic_light.rb
+++ app/models/traffic_light.rb
@@ -1,3 +1,13 @@
+class CheckUpdatedAt
+  def initialize(record)
+    @record = record
+  end
+
+  def call
+    @record.updated_at < 10.seconds.ago
+  end
+end
+
 class TrafficLight < ApplicationRecord
   include AASM
 
@@ -7,17 +17,21 @@ class TrafficLight < ApplicationRecord
     state :yellow_to_red, display: I18n.t("traffic_lights.aasm.yellow_to_red")
     state :green, display: I18n.t("traffic_lights.aasm.green")
 
-    event :prepare do
-      transitions from: :red, to: :yellow_to_green
-      transitions from: :green, to: :yellow_to_red
+    event :prepare, guard: :updated_more_than_10_secs_ago? do
+      transitions from: :red, to: :yellow_to_green, guard: CheckUpdatedAt
+      transitions from: :green, to: :yellow_to_red, if: :updated_more_than_10_secs_ago?
     end
 
     event :go do
-      transitions from: :yellow_to_green, to: :green
+      transitions from: :yellow_to_green, to: :green, guard: :updated_more_than_10_secs_ago?
     end
 
     event :stop do
-      transitions from: :yellow_to_red, to: :red
+      transitions from: :yellow_to_red, to: :red, guard: :updated_more_than_10_secs_ago?
     end
+  end
+
+  def updated_more_than_10_secs_ago?
+    updated_at < 10.seconds.ago
   end
 end
--- spec/models/traffic_light_spec.rb
+++ spec/models/traffic_light_spec.rb
@@ -2,25 +2,30 @@ require "rails_helper"
 
 RSpec.describe TrafficLight, type: :model do
   it do
-    traffic_light = TrafficLight.new
+    traffic_light = TrafficLight.create
 
+    traffic_light.updated_at = 20.seconds.ago
     expect(traffic_light).to be_red
     expect(traffic_light.aasm.human_state).to eq "stop"
 
-    traffic_light.prepare
+    traffic_light.updated_at = 20.seconds.ago
+    traffic_light.prepare!
     expect(traffic_light).to be_yellow_to_green
     expect(traffic_light.aasm.human_state).to eq "prepare to go"
 
     expect { traffic_light.prepare }.to raise_error(AASM::InvalidTransition)
     expect(traffic_light.may_prepare?).to be_falsey
 
-    traffic_light.go
+    traffic_light.updated_at = 20.seconds.ago
+    traffic_light.go!
     expect(traffic_light).to be_green
 
-    traffic_light.prepare
+    traffic_light.updated_at = 20.seconds.ago
+    traffic_light.prepare!
     expect(traffic_light).to be_yellow_to_red
 
-    traffic_light.stop
+    traffic_light.updated_at = 20.seconds.ago
+    traffic_light.stop!
     expect(traffic_light).to be_red
   end
 end
